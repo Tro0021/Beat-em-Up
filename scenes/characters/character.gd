be108fa494b672bca26513ad52491c3a8030dc99
Tro0021 <abhirambellamkonda80@gmail.com>
2026-03-08 01:17:52 +0530

    2 Hours Mark

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -6,36 +6,60 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var character_sprite: Sprite2D = $CharacterSprite
+@onready var damage_emitter: Area2D = $DamageEmitter
 
-enum State {IDLE, WALK}
+enum State {IDLE, WALK, ATTACK}
 
 var state = State.IDLE
+
+func _ready() -> void:
+	damage_emitter.area_entered.connect(on_emit_damage.bind())
 
 func _process(_delta: float) -> void:
 	handle_input()
 	handle_movement()
 	handle_aniamtion()
-	fli_sprites()
+	flip_sprites()
 	move_and_slide()
 
 func handle_movement():
-	if velocity.length() == 0:
-		state = State.IDLE
+	if can_move():
+		if velocity.length() == 0:
+			state = State.IDLE
+		else:
+			state = State.WALK
 	else:
-		state = State.WALK
+		velocity = Vector2.ZERO
 
 func handle_input() -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
+	if can_attack() and Input.is_action_just_pressed("attack"):
+		state = State.ATTACK
 
 func handle_aniamtion() -> void:
 	if state == State.IDLE:
 		animation_player.play("idle")
 	elif state == State.WALK:
 		animation_player.play("walk")
+	elif state == State.ATTACK:
+		animation_player.play("punch")
 
-func fli_sprites() -> void:
+func flip_sprites() -> void:
 	if velocity.x > 0:
 		character_sprite.flip_h = false
 	elif velocity.x < 0:
 		character_sprite.flip_h = true
+
+func can_move() -> bool:
+	return state == State.IDLE or state == State.WALK
+
+func can_attack() -> bool:
+	return state == State.IDLE or state == State.WALK
+
+func on_action_complete() -> void:
+	state = State.IDLE
+
+func on_emit_damage(damage_receiver : Area2D) -> void:
+	damage_receiver.get_parent().get_damage()
+	print(damage_receiver)
