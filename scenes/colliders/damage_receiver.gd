602c5363cb2fd79003ee46283ee2b1e2b208e0f2
Tro0021 <abhirambellamkonda80@gmail.com>
2026-03-08 11:04:00 +0530

    3 hours

--- scenes/colliders/damage_receiver.gd
+++ scenes/colliders/damage_receiver.gd
@@ -1,11 +1,4 @@
+class_name DamageReceiver
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+signal damage_received(damage : int)
